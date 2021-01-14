e44aebbf8d71dd0c0574b66ef20f57a4ce963cfa
Alexandre Santos <alexandresoad@hotmail.com>
2021-01-14 17:13:21 -0300

    add wiki module (blueprint and orm model)

--- app/models/wiki.py
+++ app/models/wiki.py
@@ -11,9 +11,24 @@ class Article(db.Model):
     text = db.Column(db.Text, index=False, nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'))
     def __repr__(self):
         return f'<Article {self.title}>'
 
     def get_description_html(self):
         return markdown(self.text)
-    
+
+class Topic(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), index=True, nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    articles = db.relationship('Article', backref='topic', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<Topic {self.name}>'
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(48), index=True, nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    # articles = db.relationship('Article', )
