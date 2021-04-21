c2b89997eb4e0f13406702f19d0819ec8f9474e1
Alexandre Santos <alexandresoad@hotmail.com>
2021-04-20 21:26:49 -0300

    u_e

--- app/blueprints/api.py
+++ app/blueprints/api.py
@@ -6,22 +6,14 @@ from app.core.db import db
 from app.models.wiki import Question, QuestionLike, QuestionSave, QuestionView, Tag
 from app.models.security import User
 from app.models.search import Search
-from app.utils.routes import counter
-from app.utils.kernel import order_dict
 # import time
 
 bp = Blueprint('api', __name__, url_prefix='/api/')
 
-# @bp.before_request
-# @counter
-# def before_request():
-#     print('aqui before_request')
-#     pass
 
 @bp.route('question/<int:id>', methods=['GET', 'POST'])
-@counter
 def question(id):
-    question = Question.query.filter(Question.id==id).first_or_404()
+    question = Question.query.filter(Question.id == id).first_or_404()
     if question.answer_approved == False:
         abort(404)
     to_dict = question.to_dict()
@@ -45,36 +37,41 @@ def question(id):
     return jsonify(to_dict)
 
 
+@bp.route('json/questions/access')
+def question_access():
+    return ''
+
+
 # api dashboard
 @bp.route('dashboard/tags_data', methods=['GET', 'POST'])
 def tags_data():
     '''
     Gera dados das 10 maiores categorias, caso tenham mais de 10 categorias a 10ª será Outros
     '''
-    questions = order_dict(Tag._dict_count_questions(), size= 10)
+    questions = Tag._dict_count_questions()
     return jsonify({
         'labels': list(questions.keys()),
         'datasets': [{
             'data': list(questions.values()),
             'backgroundColor': [
-                "#BDC3C7",
-                "#9B59B6",
-                "#E74C3C",
-                "#26B99A",
-                "#3498DB"
-                "#ed8a34",
-                "#43fad5",
-                "#4a2a38",
-                "#362b4f",
-                "#3b8a3b",
-            ]#,
-            # 'hoverBackgroundColor': [
-            #     "#CFD4D8",
-            #     "#B370CF",
-            #     "#E95E4F",
-            #     "#36CAAB",
-            #     "#49A9EA"
-            # ]
+                "blue",
+                "purple",
+                "pink",
+                "red",
+                "orange",
+                "yellow",
+                "green",
+                "cyan",
+                "gray",
+                "black"
+            ],
+            'hoverBackgroundColor': [
+                "#CFD4D8",
+                "#B370CF",
+                "#E95E4F",
+                "#36CAAB",
+                "#49A9EA"
+            ]
         }]
     })
     {
