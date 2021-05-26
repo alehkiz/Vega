0909466e1c9621b3be40775f7bc54b874fd868f8
alehkiz <alexandresoad@hotmail.com>
2021-05-26 00:20:59 -0300

    requirements

--- app/core/db.py
+++ app/core/db.py
@@ -1,8 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import SQLAlchemyUserDatastore
+# from flask_security import SQLAlchemySessionUserDatastore
 
 db = SQLAlchemy(session_options={'autoflush':False})
 
 from app.models.security import User, Role
 
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+# user_datastore = SQLAlchemySessionUserDatastore(db, User, Role)
