7f53a9500ad4c2b765e6e6c23c48328208782ff2
alehkiz <alexandresoad@hotmail.com>
2021-06-27 23:12:21 -0300

    docker

--- app/core/db.py
+++ app/core/db.py
@@ -16,4 +16,32 @@ user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 def init_db_command():
     """Clear the existing data and create new tables."""
     db.create_all()
-    click.echo('Initialized the database.')
+    click.echo('Banco de dados inicializado...')
+    from app.models.wiki import Topic
+    from app.models.security import User
+    atendimento = Topic.query.filter(Topic.name == 'Atendimento').first()
+    if atendimento is None:
+        atendimento = Topic()
+        atendimento.name = 'Atendimento'
+        atendimento.format_name = 'Atendimento'
+        atendimento.selectable = True
+        db.session.add(atendimento)
+    retaguarda = Topic.query.filter(Topic.name == 'Retaguarda').first()
+    if retaguarda is None:
+        retaguarda = Topic()
+        retaguarda.name = 'Retaguarda'
+        retaguarda.format_name = 'Retaguarda'
+        retaguarda.selectable = True
+        db.session.add(retaguarda)
+    
+    db.session.commit()
+
+    click.echo('Topicos iniciados')
+
+    anon = User()
+    anon.username = 'ANON'
+    anon.name = 'ANON'
+    anon.email = 'anon@localhost'
+    anon.created_ip = '0.0.0.0'
+    anon._password = '123'
+    anon.temp_password = True
