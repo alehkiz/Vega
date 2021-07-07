97f0a2bdcb6aae8d3f3df9b840bfa77b7587f248
alehkiz <alexandresoad@hotmail.com>
2021-07-07 01:34:58 -0300

    reset_password

--- app/blueprints/auth.py
+++ app/blueprints/auth.py
@@ -1,5 +1,6 @@
 from logging import error
 from flask import current_app as app, Blueprint, render_template, url_for, redirect, flash, json, request, g, session as fsession
+from flask_login.utils import login_required
 from flask_security import current_user, login_user, logout_user
 from sqlalchemy.orm import session
 from werkzeug.urls import url_parse
@@ -21,16 +22,11 @@ def before_request():
 
 @bp.route('/login/', methods=['GET', 'POST'])
 def login():
-    
     if current_user.is_authenticated:
         return redirect(url_for('main.index'))
     login = LoginForm()
     if login.validate_on_submit():
         user = User.query.filter_by(username=login.username.data).first()
-        # print(app.config['SECURITY_PASSWORD_SALT'])
-        # print(user)
-        # print(login.password.data)
-        # print(user.check_password(login.password.data))
         if user is None or not user.check_password(login.password.data):
             flash('Senha ou usuário inválido', category='danger')
             return render_template('login.html', form=login, title='Login')#redirect(url_for('auth.login'))
@@ -41,7 +37,7 @@ def login():
             flash('É necessário alterar sua senha.', category='info')
             
             fsession['temp_user'] = user.username
-            return redirect(url_for('auth.change_password'))
+            return redirect(url_for('auth.temp_password'))
         login_user(user, remember=login.remember_me.data)
         user.last_login_ip = request.remote_addr
         user.last_login_at = datetime.utcnow()
@@ -62,7 +58,10 @@ def login():
     return render_template('login.html', form=login, title='Login')
 
 @bp.route('/temp_password/', methods=['GET', 'POST'])
-def change_password():
+def temp_password():
+    if current_user.is_authenticated:
+        flash(message='Usuário logado', category='danger')
+        return redirect(url_for('auth.login'))
     username = fsession.get('temp_user', False)
     if username is False:
         message = 'Nenhum usuário encontrado'
@@ -82,12 +81,17 @@ def change_password():
     form = ResetPassword()
     form.username.data = user.username
     if form.validate_on_submit():
-        
         if form.new_password.data == form.confirm_new_password.data:
+            if not user.check_password(form.old_password.data):
+                # senha antiga não é igual a senha do usuário
+                message = 'Senha atual incorreta'
+                flash(message=message, category='danger')
+                return redirect(url_for('auth.login'))
             user.password = form.new_password.data
             user.temp_password = False
             try:
                 db.session.commit()
+                flash(message='Senha alterada com sucesso, acesse novamente', category='info')
                 return redirect(url_for('auth.login'))
             except Exception as e:
                 app.logger.error(app.config.get('_ERRORS').get('DB_COMMIT_ERROR'))
@@ -96,9 +100,38 @@ def change_password():
         else:
             form.confirm_new_password.errors.append('Senha não confere com a senha informada.')
             return render_template('reset_password.html', form=form)
-    print(fsession['temp_user'])
-    
+    return render_template('reset_password.html', form=form)
 
+@bp.route('/change_password/', methods=['GET', 'POST'])
+@login_required
+def change_password():
+    
+    if not current_user.is_authenticated:
+        flash(message='Usuário não logado', category='danger')
+        return redirect(url_for('main.index'))
+    
+    form = ResetPassword()
+    form.username.data = current_user.username
+    if form.validate_on_submit():
+        if form.new_password.data == form.confirm_new_password.data:
+            if not current_user.check_password(form.old_password.data):
+                # senha antiga não é igual a senha do usuário
+                message = 'Senha atual incorreta'
+                flash(message=message, category='danger')
+                return redirect(url_for('auth.login'))
+            current_user.password = form.new_password.data
+            current_user.temp_password = False
+            try:
+                db.session.commit()
+                flash(message='Senha alterada com sucesso, acesse novamente', category='info')
+                return redirect(url_for('auth.login'))
+            except Exception as e:
+                app.logger.error(app.config.get('_ERRORS').get('DB_COMMIT_ERROR'))
+                app.logger.error(e)
+                return redirect(url_for('auth.login'))
+        else:
+            form.confirm_new_password.errors.append('Senha não confere com a senha informada.')
+            return render_template('reset_password.html', form=form)
     return render_template('reset_password.html', form=form)
 
 @bp.route('/logout/')
