a8d16454d63f01ce367fb16ed5ca52b4f65e8460
alehkiz <alexandresoad@hotmail.com>
2021-08-26 00:24:02 -0300

    notifier

--- app/models/notifier.py
+++ app/models/notifier.py
@@ -14,11 +14,15 @@ class Notifier(db.Model):
     title = db.Column(db.String(128), index=True, nullable=False, unique=False)
     content = db.Column(db.String, index=True, nullable=False)
     status_id = db.Column(db.Integer, db.ForeignKey('notifier_status.id'), nullable=False)
+    priority_id = db.Column(db.Integer, db.ForeignKey('notifier_priority.id'), nullable=False)
     create_at = db.Column(db.DateTime, index=False, default=datetime.utcnow)
     created_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     topic_id = db.Column(db.Integer, db.ForeignKey('topic.id'), nullable=False)
     # status = db.relationship('NotifierStatus', single_parent=True, backref='notices', lazy='dynamic')
     
+    @property
+    def get_create_time_elapsed(self):
+        return format_elapsed_time(self.create_at)
 
     def __repr__(self) -> str:
         return super().__repr__()
@@ -27,3 +31,8 @@ class NotifierStatus(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.String(32), nullable=False)
     notices = db.relationship('Notifier', backref='status', lazy='dynamic')
+
+class NotifierPriority(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    priority = db.Column(db.String(32), nullable=False)
+    notices = db.relationship('Notifier', backref='priority', lazy='dynamic') 
