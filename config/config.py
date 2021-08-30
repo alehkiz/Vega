033f1c4c8b985203f4644135df1d25f566c41f7c
Alexandre Santos <alexandresoad@hotmail.com>
2021-08-30 12:54:57 -0300

    publish

--- config/config.py
+++ config/config.py
@@ -34,8 +34,9 @@ class BaseConfig(object):
 
     _SQLALCHEMY_DATABASE_NAME = PROJECT_NAME.lower()
     _SQLALCHEMY_DATABASE_HOST = environ.get('DB_HOST') or 'localhost'
-    _SQLALCHEMY_DATABASE_USERNAME = environ.get('DB_USER')
+    _SQLALCHEMY_DATABASE_USERNAME = environ.get('DB_USER') or 'postgres'
     _SQLALCHEMY_DATABASE_PASSWORD = environ.get('DB_PASS')
+    _SQLALCHEMY_DATABASE_PORT = environ.get('DB_PORT')
 
     _ERRORS = {'DB_COMMIT_ERROR': 'Não foi possível atualizar o banco de dados'}
 
@@ -171,7 +172,7 @@ class BaseConfig(object):
 
 class DevelopmentConfig(BaseConfig):
     # SQLALCHEMY_DATABASE_URI = f'sqlite:///{BaseConfig.DEV_DB}'
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{BaseConfig._SQLALCHEMY_DATABASE_USERNAME}:{BaseConfig._SQLALCHEMY_DATABASE_PASSWORD}@{BaseConfig._SQLALCHEMY_DATABASE_HOST}/{BaseConfig._SQLALCHEMY_DATABASE_NAME}'
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{BaseConfig._SQLALCHEMY_DATABASE_USERNAME}:{BaseConfig._SQLALCHEMY_DATABASE_PASSWORD}@{BaseConfig._SQLALCHEMY_DATABASE_HOST}:{BaseConfig._SQLALCHEMY_DATABASE_PORT}/{BaseConfig._SQLALCHEMY_DATABASE_NAME}'
     MODE = 'dev'
     ENV = 'dev'
 
@@ -183,7 +184,7 @@ class TestConfig(BaseConfig):
 
 class ProductionConfig(BaseConfig):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{BaseConfig._SQLALCHEMY_DATABASE_USERNAME}:{BaseConfig._SQLALCHEMY_DATABASE_PASSWORD}@{BaseConfig._SQLALCHEMY_DATABASE_HOST}/{BaseConfig._SQLALCHEMY_DATABASE_NAME}'
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{BaseConfig._SQLALCHEMY_DATABASE_USERNAME}:{BaseConfig._SQLALCHEMY_DATABASE_PASSWORD}@{BaseConfig._SQLALCHEMY_DATABASE_HOST}:{BaseConfig._SQLALCHEMY_DATABASE_PORT}/{BaseConfig._SQLALCHEMY_DATABASE_NAME}'
     # SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or f'postgresql://khtknuvbaeavvq:98b557036b61944f2912ccc6aa07b0c907352da55603ce611bb9b744da9398fa@ec2-23-23-128-222.compute-1.amazonaws.com:5432/dtf9faocttt57'
     SESSION_COOKIE_SECURE = False
     MODE = 'prod'
