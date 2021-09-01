2827391e36d13575ba1cda60b151b14e66b21152
alehkiz <alexandresoad@hotmail.com>
2021-08-31 23:21:27 -0300

    Nofitier order

--- app/blueprints/notifier.py
+++ app/blueprints/notifier.py
@@ -9,6 +9,6 @@ bp = Blueprint('notifier', __name__, url_prefix='/notificacao')
 def index():
     notices_active = db.session.query(Notifier).join(NotifierStatus, Notifier.status).join(NotifierPriority, Notifier.priority).filter(NotifierStatus.status == 'Ativo').order_by(NotifierPriority.id.asc())
     # Notifier.query.filter(NotifierStatus.status == 'Ativo')
-    notices_history = db.session.query(Notifier).join(NotifierStatus, Notifier.status).filter(NotifierStatus.status == 'Histórico').order_by(NotifierPriority.id.asc())
+    notices_history = db.session.query(Notifier).join(NotifierStatus, Notifier.status).join(NotifierPriority, Notifier.priority).filter(NotifierStatus.status == 'Histórico').order_by(NotifierPriority.id.asc())
     # Notifier.query.filter(NotifierStatus.status == 'Histórico')
     return render_template('notifier.html', notices_active = notices_active, notices_history = notices_history)
