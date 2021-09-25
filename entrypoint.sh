7cab547aa28d2caa864e7ba5fe9cf98032afefbe
Alexandre Santos <alexandresoad@hotmail.com>
2021-09-25 17:06:45 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-echo $SQL_HOST
+echo $DB_HOST
 if [ "$DATABASE" = "vega" ]
 then
     echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
+    while ! nc -z $DB_HOST $DB_PORT; do
       sleep 0.1
     done
 
