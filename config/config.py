327900a442a7be77ce96f9a43b713786167c41f2
Alexandre Santos <alexandresoad@hotmail.com>
2021-10-04 14:15:51 -0300

    has_support

--- config/config.py
+++ config/config.py
@@ -9,7 +9,7 @@ import app
 
 class BaseConfig(object):
     PROJECT_NAME = 'Vega'
-    SITE_TITLE = 'ResponDetran'
+    SITE_TITLE = 'AtenDetran'
     SECRET_KEY = environ.get(
         'SERVER_KEY')
     APP_DIR = abspath(dirname(app.__file__))
