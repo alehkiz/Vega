c3ba2f415350b962de3be3e4af09c79ec04ed893
alehkiz <alexandresoad@hotmail.com>
2022-01-09 22:34:34 -0300

    Update wiki.py

--- app/models/wiki.py
+++ app/models/wiki.py
@@ -882,7 +882,7 @@ class QuestionChanges(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     create_at = db.Column(db.DateTime, index=False, default=datetime.utcnow)
-    change_user_id = db.Column(db.Integer, db.ForeignKey('user.ud'), nullable=False)
+    change_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     old_answer = db.Column(db.Text, index=False, nullable=False, unique=False)
     new_answer = db.Column(db.Text, index=False, nullable=False, unique=False)
     
