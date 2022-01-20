af5c004820fb296620701b9927aedba3b0a75167
Alexandre Santos <alexandresoad@hotmail.com>
2022-01-20 10:23:08 -0300

    Update interval

--- app/dashboard/index.py
+++ app/dashboard/index.py
@@ -358,7 +358,7 @@ def dash_app(app=False):
             ], id='tabs', active_tab='diary'),
             html.Div(id='tab_content', className='p-4'),
             dcc.Interval(id='interval-component',
-                         interval=30000,  # 25 segundos
+                         interval=30000,  # 30 segundos
                          n_intervals=0)  # contador para atualização
         ])
 
