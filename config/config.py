e5b522070e4feb3bf9905e96b311b1f1c807d04c
Alexandre Santos <alexandresoad@hotmail.com>
2022-03-18 18:47:19 -0300

    word upload

--- config/config.py
+++ config/config.py
@@ -269,7 +269,7 @@ class BaseConfig(object):
     USER_ANON_ID = 4
     UPLOAD_FOLDER = join(APP_DIR, 'upload')
     MAX_CONTENT_LENGTH = 10485760 #20MB
-    UPLOAD_EXTENSIONS = ['pdf']
+    UPLOAD_EXTENSIONS = ['pdf', 'doc', 'docx']
 
 class DevelopmentConfig(BaseConfig):
     # SQLALCHEMY_DATABASE_URI = f'sqlite:///{BaseConfig.DEV_DB}'
