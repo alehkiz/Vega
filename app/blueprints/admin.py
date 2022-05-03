1e57cab51e815cf5457f432dd6d2c688c45ac7b7
Alexandre Santos <alexandresoad@hotmail.com>
2022-05-03 18:38:43 -0300

    file type

--- app/blueprints/admin.py
+++ app/blueprints/admin.py
@@ -790,8 +790,11 @@ def file():
     )
     last_page = paginate.pages
     order_type = "asc" if order_type == "desc" else "desc"
-
+    order_type_inverse = "asc" if order_type == "desc" else "desc"
+    order = request.args.get('order', None)
     url_args = dict(request.args)
+    url_args.pop('order_type', None)
+    url_args.pop('order', None)
     url_args.pop('page') if 'page' in url_args.keys() else None
 
     return render_template(
@@ -808,5 +811,7 @@ def file():
         _topic=Topic.query.filter(Topic.selectable == True),
         _sub_topic=SubTopic.query,
         request_args=request_args,
-        url_args=url_args
+        url_args=url_args,
+        order_type_inverse = order_type_inverse,
+        order=order
     )
