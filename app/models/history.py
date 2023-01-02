dd9338cd84e76f20815e32493454168e310c420f
Alexandre Santos <alexandresoad@hotmail.com>
2023-01-02 18:21:58 -0300

    add-question-id

--- app/models/history.py
+++ app/models/history.py
@@ -1,16 +1,20 @@
+from datetime import datetime
 from app.core.db import db
-
+from app.utils.kernel import convert_datetime_to_local
 
 
 class QuestionHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(
+        db.Integer, db.ForeignKey('question.id'), nullable=False)
     question = db.Column(db.String(800), index=False,
                          nullable=False, unique=True)
     answer = db.Column(db.Text, index=False,
-                        nullable=True, unique=False)
+                       nullable=True, unique=False)
     answer_approved = db.Column(db.Boolean, nullable=True, default=False)
     answer_approved_at = db.Column(db.DateTime(timezone=True))
-    create_at = db.Column(db.DateTime(timezone=True), index=False, default=convert_datetime_to_local(datetime.utcnow()))
+    create_at = db.Column(db.DateTime(timezone=True), index=False,
+                          default=convert_datetime_to_local(datetime.utcnow()))
     create_user_id = db.Column(
         db.Integer, db.ForeignKey('user.id'), nullable=False)
     update_at = db.Column(db.DateTime(timezone=True))
@@ -23,3 +27,6 @@ class QuestionHistory(db.Model):
     answer_network_id = db.Column(
         db.Integer, db.ForeignKey('network.id'), nullable=True)
     active = db.Column(db.Boolean, nullable=False)
+
+    def __repr__(self) -> str:
+        return f'<Question {self.question_id}>'
