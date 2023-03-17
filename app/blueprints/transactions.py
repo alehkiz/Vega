7979c75bbfd1e405deced4c66041e4a45c2542ab
Alexandre Santos <alexandresoad@hotmail.com>
2023-03-16 23:26:52 -0300

    transactions

--- app/blueprints/transactions.py
+++ app/blueprints/transactions.py
@@ -1,6 +1,11 @@
-from flask import Blueprint
+from datetime import datetime
+from flask import Blueprint, flash, render_template, redirect, url_for, current_app as app, request
 from flask_login import login_required
-from flask_security import roles_accepted
+from flask_security import login_required, current_user, roles_accepted
+from app.forms.transaction import TransactionForm, TransactionOptionForm, TransactionScreenForn
+from app.models.transactions import Transaction, TransactionOption
+from app.utils.kernel import convert_datetime_to_local
+from app.core.db import db
 
 from app.utils.routes import counter
 bp = Blueprint('transactions', __name__, url_prefix='/transacoes')
@@ -13,11 +18,63 @@ def index():
     return 'none'
 
 
-@bp.route('/add')
+@bp.route('/add', methods=['GET', 'POST'])
 @login_required
 @roles_accepted('admin', 'support')
 def add():
-    return 'none'
+    if request.args.get('id', False) != False and request.args.get('options', False) != False:
+        form = TransactionOptionForm()
+        if not request.args.get('id', False).isnumeric():
+            flash('Não foi possível identificar a transação', category='danger')
+            return render_template('add.html', options=True)
+        t_id = int(request.args.get('id', False))
+        transaction = Transaction.query.filter(Transaction.id == t_id).first()
+        if transaction is None:
+            flash('Não foi possível identificar a transação', category='danger')
+            return render_template('add.html', options=True)
+        
+        if form.validate_on_submit():
+            
+            option = TransactionOption()
+            print(form.options)
+        form.transaction.data = transaction.transaction
+        return render_template('add.html', form=form, title='Adicionar', options=True)
+    
+    
+    
+    form = TransactionForm()
+    if form.validate_on_submit():
+        transaction = Transaction.query.filter(Transaction.transaction.ilike(form.transaction.data)).first()
+        if transaction != None:
+            # flash(f'Transação {form.transaction.data} já está cadastrada.', category='danger')
+            form.transaction.errors.append(f'Transação {form.transaction.data} já está cadastrada.')
+            return render_template('add.html', form=form, title='Adicionar', transaction=True)            
+        transaction = Transaction()
+        transaction.transaction = form.transaction.data
+        transaction.parameters.extend(form.parameter.data)
+        transaction.description = form.description.data
+        transaction.type_id = form.type.data[0].id
+        transaction.topics.extend(form.topic.data)
+        transaction.sub_topics.extend(form.sub_topics.data)
+        transaction.created_user_id = current_user.id
+        transaction.create_at = convert_datetime_to_local(datetime.utcnow())
+
+        try:
+            db.session.add(transaction)
+            db.session.commit()
+            if request.form.get('submit') == 'Salvar':
+                return redirect(url_for('transactions.id', id=transaction.id))
+            elif request.form.get('submit') == 'Prints':
+                return redirect(url_for('transactions.add', id=transaction.id, options=True))
+            else:
+                return redirect(url_for('transactions.add', id=transaction.id, options=True))
+        except Exception as e:
+                app.logger.error(app.config.get("_ERRORS").get("DB_COMMIT_ERROR"))
+                app.logger.error(e)
+                db.session.rollback()
+                return render_template('add.html', form=form, title='Adicionar', transaction=True)
+
+    return render_template('add.html', form=form, title='Adicionar', transaction=True)
 
 @bp.route('/edit')
 @login_required
--- app/forms/transaction.py
+++ app/forms/transaction.py
@@ -1,25 +1,35 @@
 from flask.globals import request
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, StringField, SubmitField, BooleanField
+from wtforms import TextAreaField, StringField, SubmitField, BooleanField, MultipleFileField, FieldList
 from wtforms.validators import DataRequired, Length
 from wtforms.ext.sqlalchemy.fields import QuerySelectField, QuerySelectMultipleField
-from app.models.transactions import TransactionParameter, TransactionType
+from app.models.transactions import Parameter, TransactionType
 from app.models.wiki import SubTopic, Topic, Tag
 from app.models.security import User
 from flask import request
 
 
 class TransactionForm(FlaskForm):
-    transaction = TextAreaField('Transação', validators=[DataRequired('Item obrigatório'), Length(min=4, max=4, message='A transação deve ter exatamente 4 caracteres')])
-    parameter = QuerySelectMultipleField('Parametros', allow_blank=False, query_factory= lambda : TransactionParameter.query, get_label = 'type')
+    transaction = StringField('Transação', validators=[DataRequired('Item obrigatório'), Length(min=4, max=4, message='A transação deve ter exatamente 4 caracteres')])
+    parameter = QuerySelectMultipleField('Parametros', allow_blank=False, query_factory= lambda : Parameter.query, get_label = 'type')
     # option = QuerySelectMultipleField('Parametros', allow_blank=False, query_factory= lambda : TransactionParameter.query, get_label = 'name', validators = [DataRequired('Item Obrigatório')])
     # tag = QuerySelectMultipleField('Tag', allow_blank=False, query_factory= lambda : Tag.query.order_by(Tag.name.asc()), get_label='name', validators=[DataRequired('Item Obrigatório')])
+    description = TextAreaField('Descrição', validators=[DataRequired('Item obrigatório'), Length(min=8, max=540, message='A descrição deve ter entre 8 e 540 caracteres')])
     type = QuerySelectMultipleField('Tipo', allow_blank=False, query_factory= lambda : TransactionType.query, get_label = 'type', validators = [DataRequired('Item Obrigatório')])
     topic = QuerySelectMultipleField('Topico', allow_blank=False, query_factory= lambda : Topic.query.filter(Topic.selectable == True), get_label = 'name', validators = [DataRequired('Item Obrigatório')])
-    sub_topic = QuerySelectMultipleField('Sub-Tópico', allow_blank=True, query_factory= lambda : SubTopic.query, get_label='name', validators=[DataRequired('Item Obrigatório')])
-    submit = SubmitField('Enviar')
+    sub_topics = QuerySelectMultipleField('Sub-Tópico', allow_blank=True, query_factory= lambda : SubTopic.query, get_label='name', validators=[DataRequired('Item Obrigatório')])
+    submit = SubmitField('Salvar')
+    prints = SubmitField('Prints')
+
 
 class TransactionOptionForm(FlaskForm):
-    transaction = TextAreaField('Transação', validators=[DataRequired('Item obrigatório'), Length(min=4, max=4, message='A transação deve ter exatamente 4 caracteres')],render_kw={'disabled':''})
-    
+    transaction = StringField('Transação', render_kw={'disabled':''})
+    options = FieldList(StringField('Opção'), min_entries=1)
+    description = TextAreaField('Descrição')
+    submit = SubmitField('Salvar')
+
+class TransactionScreenForn(FlaskForm):
+    transaction = StringField('Transação', render_kw={'disabled':''})
+    files = MultipleFileField("Arquivos")
+    submit = SubmitField('Salvar')
     
